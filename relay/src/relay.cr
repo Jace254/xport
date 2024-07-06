af90d688e728ee3f23bbefca582dac922e0aa3fb
Hypnos <47209391+Jace254@users.noreply.github.com>
2024-07-06 04:57:42 +0300

    chore: update

--- relay/src/relay.cr
+++ relay/src/relay.cr
@@ -38,7 +38,27 @@ class SafeClientList
   end
 end
 
+# class Message 
+#   @message : String
+#   @mutex : Mutex
 
+#   def initialize
+#     @message = ""
+#     @mutex = Mutex.new
+#   end
+
+#   def set(message : String)
+#     @mutex.synchronize { @message = message }
+#   end
+
+#   def get
+#     @mutex.synchronize { @message }
+#   end
+
+#   def finalize
+#     @mutex.synchronize { @message = "" }
+#   end
+# end
 
 def handle_client(client : Client, agents : SafeClientList, clients : SafeClientList)
   Log.info { "Init handler for #{client.type}" }
@@ -50,13 +70,12 @@ def handle_client(client : Client, agents : SafeClientList, clients : SafeClient
       message = client.socket.gets(chomp: false) || break
       
       if client.type == "agent"
-
         if clients.empty?
           Log.info { "No clients connected. Closing agent connection." }
           break
         elsif
           # Log.debug { "Sending message to clients (#{clients.size})" }
-          clients.each do |c| 
+          clients.each do |c|
             begin
               c.socket.puts(message)
             rescue ex : IO::Error
@@ -89,7 +108,8 @@ def handle_client(client : Client, agents : SafeClientList, clients : SafeClient
           end
         end
       end
-      
+      message = nil
+      client.socket.flush
     rescue ex : IO::Error
       Log.error(exception: ex) { "Connection error for #{client.type}: #{ex.message}" }
       break
@@ -97,7 +117,6 @@ def handle_client(client : Client, agents : SafeClientList, clients : SafeClient
       Log.error(exception: ex) { "Unexpected error for #{client.type}: #{ex.message}" }
       break
     end
-    message = nil
   end
 rescue ex
   Log.error(exception: ex) { "Exception in handle_client for #{client.type}" }
@@ -111,15 +130,15 @@ ensure
   Log.info { "#{client.type.capitalize} disconnected" }
 end
 
-server = TCPServer.new("0.0.0.0", 443)
-Log.info { "Server listening on port 443" }
+server = TCPServer.new("0.0.0.0", 8080)
+Log.info { "Server listening on port 8080" }
 
 agents  = SafeClientList.new
 clients = SafeClientList.new
 
 while socket = server.accept?
-  client_type = socket.gets(chomp: true)
-  if client_type && {"agent", "ui"}.includes?(client_type)
+  client_type = socket.gets(chomp: true) # "agent\njoash"
+  if client_type && {"agent", "ui"}.includes?(client_type) 
     client = Client.new(client_type, socket)
     (client_type == "agent" ? agents : clients).add(client)
     Log.info { "#{client_type.capitalize} client connected" }
